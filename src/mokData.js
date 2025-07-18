b90d93edbe07d98483745d1e844c8aa6af8ba585
altjs3310 <altjs3310@naver.com>
2025-07-18 16:34:46 +0900

    메인페이지, 컴포넌트 분리, 리액트라우터 지정

---
+++ src/mokData.js
@@ -0,0 +1,24 @@
+let data = [
+  {
+    id: 0,
+    title: 'apple',
+    content: '새콤달콤 맛있는 사과',
+    price: 12000
+  },
+  {
+    id: 1,
+    title: 'strawberry',
+    content: '새콤달콤 맛있는 사과',
+    price: 25000
+  },
+  {
+    id: 2,
+    title: 'watermelon',
+    content: '새콤달콤 맛있는 사과',
+    price: 35000
+  },
+
+
+]
+
+export default data;
